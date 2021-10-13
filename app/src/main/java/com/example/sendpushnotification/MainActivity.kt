25f4a558781c3d115402e361441427a8a75ed74e
Office PC <72garmartin@gmail.com>
2021-10-13 15:37:26 +0700

    send push notif to all device

---
+++ app/src/main/java/com/example/sendpushnotification/MainActivity.kt
@@ -0,0 +1,68 @@
+package com.example.sendpushnotification
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import com.example.sendpushnotification.databinding.ActivityMainBinding
+import com.example.sendpushnotification.databinding.ActivityMainBinding.inflate
+import com.google.firebase.messaging.FirebaseMessaging
+import com.google.gson.Gson
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import java.lang.Exception
+
+const val TOPIC = "/topics/myTopic"
+
+class MainActivity : AppCompatActivity() {
+
+    val TAG = "MainActivity"
+    private lateinit var binding: ActivityMainBinding
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        FirebaseMessaging.getInstance().subscribeToTopic(TOPIC)
+
+        binding.btnSend.setOnClickListener{
+            val title = binding.etTitle.text.toString()
+            val message = binding.etMessage.text.toString()
+
+            if (title.isNotEmpty() && message.isNotEmpty()) {
+                PushNotification(
+                    NotificationData(title, message),
+                    TOPIC
+                ).also {
+                    sendNotification(it)
+                }
+                Toast.makeText(this@MainActivity, "Sending push notification ...", Toast.LENGTH_SHORT).show()
+            } else {
+                Toast.makeText(this, "Please fill your title and message for push notification", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+    }
+
+
+    private fun sendNotification(notification: PushNotification) = CoroutineScope(Dispatchers.IO).launch {
+        try {
+            val response = RetrofitInstance.api.postNotification(notification)
+
+            if (response.isSuccessful) {
+//                Log.d(TAG, "sendNotification response: ${Gson().toJson(response)}")
+                Log.d(TAG, "sendNotification response: $response")
+            } else {
+                Log.e(TAG, "sendNotification error: "+response.errorBody().toString())
+            }
+
+        } catch (e: Exception) {
+            e.printStackTrace()
+            Log.e(TAG, "sendNotification: "+e.toString())
+        }
+    }
+}
---
+++ app/src/main/java/com/example/sendpushnotification/NotificationAPI.kt
@@ -0,0 +1,17 @@
+package com.example.sendpushnotification
+
+import com.example.sendpushnotification.Constants.Companion.CONTENT_TYPE
+import okhttp3.ResponseBody
+import retrofit2.Response
+import retrofit2.http.Body
+import retrofit2.http.Headers
+import retrofit2.http.POST
+
+interface NotificationAPI {
+
+    @Headers("Authorization: key=${BuildConfig.SERVER_KEY}", "Content-Type: $CONTENT_TYPE")
+    @POST("fcm/send")
+    suspend fun postNotification(
+        @Body notification: PushNotification
+    ): Response<ResponseBody>
+}
